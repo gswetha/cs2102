4416fbe4ac1799083228df0dd8dc8b7675bf2f8b
Roxanne Guo <roxanne.guo@me.com>
2014-03-19 16:05:48 +0800

    created unique pages for admin, guest, user

---
+++ application/views/admin_home_page.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+	<title>CS2102 - Music Catalogue</title>
+
+	<style type="text/css">
+	</style>
+</head>
+
+<body>
+  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Welcome, Admin!</a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="dropdown">
+              <a href="#" class="dropdown-toggle" role="button" id="drop1" data-toggle="dropdown">
+                My Account
+                <b class="caret"></b>
+              </a>
+              <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">My Profile</a></li>
+                <li role="presentation" class="divider"></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Logout</a></li>
+              </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-3 col-md-2 sidebar">
+        <ul class="nav nav-sidebar">
+          <li class="dropdown">
+              <a href="#" class="dropdown-toggle" role="button" id="drop2" data-toggle="dropdown">
+                Catalogue
+                <b class="caret"></b>
+              </a>
+              <ul class="dropdown-menu" role="menu" aria-labelledby="drop2">
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Song</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Album</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Artist</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Composer</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Genre</a></li>
+              </ul>
+          </li>
+          <li class="dropdown">
+              <a href="#" class="dropdown-toggle" role="button" id="drop3" data-toggle="dropdown">
+                Most Popular
+                <b class="caret"></b>
+              </a>
+              <ul class="dropdown-menu" role="menu" aria-labelledby="drop3">
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Song</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Album</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Artist</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Composer</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Genre</a></li>
+              </ul>
+          </li>
+          <li><a href="#">Add To Catalogue</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <h1 class="page-header">What's Trending</h1>
+
+        <div class="row placeholders">
+
+        <!-- Carousel
+        ================================================== -->
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="item active">
+              <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide">
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>Today's Hits</h1>
+                  <div class="col-xs-6 col-sm-3 placeholder">
+                    <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+                    <h4>Baby</h4>
+                    <span class="text-muted">Justin Bieber</span>
+                  </div>
+                  <div class="col-xs-6 col-sm-3 placeholder">
+                    <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+                    <h4>The Climb</h4>
+                    <span class="text-muted">Miley Cyrus</span>
+                  </div>
+                  <div class="col-xs-6 col-sm-3 placeholder">
+                    <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+                    <h4>Call Me Maybe</h4>
+                    <span class="text-muted">Carly Rae Jepsen</span>
+                  </div>
+                  <div class="col-xs-6 col-sm-3 placeholder">
+                    <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+                    <h4>My Love</h4>
+                    <span class="text-muted">Justin Timberlake</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>One more for good measure.</h1>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+          <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div><!-- /.carousel -->
+        </div>
+
+        <h2 class="sub-header">Music Catalogue</h2>
+        <form class="navbar-form navbar-right">
+          <input type="text" class="form-control" placeholder="Search...">
+        </form>
+        <div class="table-responsive">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Song Title</th>
+                <th>Composer</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th>Genre</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1,003</td>
+                <td>Integer</td>
+                <td>nec</td>
+                <td>odio</td>
+                <td>adipiscing</td>
+                <td><button>Delete</button></td>
+              </tr>
+              <tr>
+                <td>1,003</td>
+                <td>libero</td>
+                <td>Sed</td>
+                <td>cursus</td>
+                <td>adipiscing</td>
+                <td><button>Delete</button></td>
+              </tr>
+              <tr>
+                <td>1,004</td>
+                <td>dapibus</td>
+                <td>diam</td>
+                <td>Sed</td>
+                <td>adipiscing</td>
+                <td><button>Delete</button></td>
+              </tr>
+              <tr>
+                <td>1,005</td>
+                <td>Nulla</td>
+                <td>quis</td>
+                <td>sem</td>
+                <td>adipiscing</td>
+                <td><button>Delete</button></td>
+              </tr>
+              <tr>
+                <td>1,006</td>
+                <td>nibh</td>
+                <td>elementum</td>
+                <td>imperdiet</td>
+                <td>adipiscing</td>
+                <td><button>Delete</button></td>
+              </tr>
+              <tr>
+                <td>1,007</td>
+                <td>sagittis</td>
+                <td>ipsum</td>
+                <td>Praesent</td>
+                <td>adipiscing</td>
+                <td><button>Delete</button></td>
+              </tr>
+              <tr>
+                <td>1,008</td>
+                <td>Fusce</td>
+                <td>nec</td>
+                <td>tellus</td>
+                <td>adipiscing</td>
+                <td><button>Delete</button></td>
+              </tr>
+              <tr>
+                <td>1,009</td>
+                <td>augue</td>
+                <td>semper</td>
+                <td>adipiscing</td>
+                <td>porta</td>
+                <td><button>Delete</button></td>
+              </tr>
+              <tr>
+                <td>1,010</td>
+                <td>massa</td>
+                <td>Vestibulum</td>
+                <td>lacinia</td>
+                <td>adipiscing</td>
+                <td><button>Delete</button></td>
+              </tr>
+              <tr>
+                <td>1,011</td>
+                <td>eget</td>
+                <td>nulla</td>
+                <td>Class</td>
+                <td>adipiscing</td>
+                <td><button>Delete</button></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>

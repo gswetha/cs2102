1e5a1198bb759ff650486625de6d1613ee43e27c
Roxanne Guo <roxanne.guo@me.com>
2014-03-19 16:05:16 +0800

    dropdown menu done

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -5,10 +5,27 @@ class Home extends CI_Controller {
 	/**
    * Landing page for CS2102 project - Music DB app
 	 */
-	public function index()
-	{
+	public function index() {
 		$this->load->view('_home_header_styles');
-		$this->load->view('home_page');
+		$this->load->view('guest_home_page');
+		$this->load->view('_home_footer_script');
+	}
+
+	public function guest() {
+		$this->load->view('_home_header_styles');
+		$this->load->view('guest_home_page');
+		$this->load->view('_home_footer_script');
+	}
+
+	public function user() {
+		$this->load->view('_home_header_styles');
+		$this->load->view('user_home_page');
+		$this->load->view('_home_footer_script');
+	}
+
+	public function admin() {
+		$this->load->view('_home_header_styles');
+		$this->load->view('admin_home_page');
 		$this->load->view('_home_footer_script');
 	}
 }

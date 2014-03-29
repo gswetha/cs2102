754dfcc438ac71dd540c7c43a42728a2c0e16b7e
jsnywz <jsnywz@gmail.com>
2014-03-29 11:02:51 +0800

    Revert "Ui Stuff"
    This reverts commit 1a6bcb759c7034ec639743ac00f13c72e5f30d89.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -25,10 +25,12 @@ class Home extends CI_Controller {
 	{
 		$data['title'] = "home page";
 		$data['username'] = "Guest";
+		$data['role'] = "guest";
 		if ($this->isLoggedIn()) {
 			$data['logged_in'] = TRUE;
 			log_message('info','email of user is '.print_r($this->session->all_userdata(),TRUE));
 			$data['username'] = $this->session->userdata('name');
+			$data['role'] = $this->session->userdata('role');
 		}
 		else
 			$data['logged_in'] = FALSE;
@@ -55,69 +57,6 @@ class Home extends CI_Controller {
 		$this->load->view('admin_home_page');
 		$this->load->view('_home_footer_script');
 	}
-
-	public function songmenu() {
-		$this->load->view('_home_header_styles');
-		$this->load->view('songmenu');
-		$this->load->view('_home_footer_script');
-	}
-
-	public function albummenu()
-	{
-		$this->load->view('_home_header_styles');
-		$this->load->view('albummenu');
-		$this->load->view('_home_footer_script');
-	}
-
-	public function singermenu()
-	{
-		$this->load->view('_home_header_styles');
-		$this->load->view('singermenu');
-		$this->load->view('_home_footer_script');
-	}
-
-	public function composermenu()
-	{
-		$this->load->view('_home_header_styles');
-		$this->load->view('composermenu');
-		$this->load->view('_home_footer_script');
-	}
-
-	public function genremenu()
-	{
-		$this->load->view('_home_header_styles');
-		$this->load->view('genremenu');
-		$this->load->view('_home_footer_script');
-	}
-
-	public function singlealbumview()
-	{
-		$this->load->view('_home_header_styles');
-		$this->load->view('singlealbumview');
-		$this->load->view('_home_footer_script');
-	}
-
-	public function top10songs()
-	{
-		$this->load->view('_home_header_styles');
-		$this->load->view('top10songs');
-		$this->load->view('_home_footer_script');
-	}
-
-	public function top10albums()
-	{
-		$this->load->view('_home_header_styles');
-		$this->load->view('top10albums');
-		$this->load->view('_home_footer_script');
-	}
-
-	public function top10singers()
-	{
-		$this->load->view('_home_header_styles');
-		$this->load->view('top10singers');
-		$this->load->view('_home_footer_script');
-	}
-
 
 	function isLoggedIn(){
 		if($this->session->userdata('status') == 'logged_in')

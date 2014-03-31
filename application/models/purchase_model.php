f3bdd84be996e9e29182a59dab7f13620661a633
Swetha <gswetha@hotmail.com>
2014-03-31 20:33:38 +0800

    purchase model (not fully complete)

--- application/models/purchase_model.php
+++ application/models/purchase_model.php
@@ -10,19 +10,76 @@ class Purchase_model extends CI_Model {
 	}
 
 	function getAllPurchases(){
+		$SQL = "SELECT * FROM purchases";
+
+		$query = $this->db->query($SQL);
+		log_message('info', 'purchase_model - get all purchases'.$this->db->last_query());
+		$result = NULL;
+		if ($query->num_rows() > 0)
+		{
+		   foreach ($query->result_array() as $row)
+		   {
+		      $result[] = $row;
+		   }
+		}
+		log_message('info', 'purchase_model - result is '.print_r($result,TRUE));
+		return $result;
 		
 	}
 
-	function getRevenue(){
-
+	function getTotalRevenue(){
+		$SQL = "SELECT SUM(p.amountPaid) FROM purchases p";
+		$query = $this->db->query($SQL);
+		log_message('info', 'purchase_model - get revenue by genre'.$this->db->last_query());
+		$result = NULL;
+		if ($query->num_rows() > 0)
+		{
+		   foreach ($query->result_array() as $row)
+		   {
+		      $result[] = $row;
+		   }
+		}
+		log_message('info', 'purchase_model - result is '.print_r($result,TRUE));
+		return $result;
 	}
 
 	function getPurchasesByUser($userEmail){
-		
+		//union of results based on paypal email and user email?
+		$SQL = "SELECT * FROM purchases p WHERE p.pEmail = '".$userEmail."'".
+				"UNION SELECT p.pAlbumTitle,p.pAlbumYear,p.pSongTitle,p.pSongYear,p.pEmail,p.transactionId,p.transactionDate,p.amountPaid 
+				FROM purchases p, user u WHERE u.paypalEmail=p.pEmail AND u.email = '".$userEmail."'";
+		$query = $this->db->query($SQL);
+		log_message('info', 'purchase_model - get purchases by user'.$this->db->last_query());
+		$result = NULL;
+		if ($query->num_rows() > 0)
+		{
+		   foreach ($query->result_array() as $row)
+		   {
+		      $result[] = $row;
+		   }
+		}
+		log_message('info', 'purchase_model - result is '.print_r($result,TRUE));
+		return $result;
 	}
 
 	function getRevenueBySinger($firstName, $lastName){
-		
+		$SQL = "SELECT sss.sssSingerFirstName, sss.sssSingerLastName, sss.sssSingerStageName, SUM(p.amountPaid) FROM singersingssong sss, purchases p WHERE 
+		sss.sssAlbumTitle = p.pAlbumTitle AND sss.sssAlbumYear = p.pAlbumYear AND
+		LOWER(sss.sssSingerFirstName) LIKE LOWER('%".$firstName."%') AND LOWER(sss.sssSingerLastName) LIKE LOWER('%".$lastName."%')
+		GROUP BY ccs.ccsComposerFirstName, ccs.ccsComposerLastName, SUM(p.amountPaid) ORDER BY SUM(p.amountPaid) DESC";
+
+		$query = $this->db->query($SQL);
+		log_message('info', 'purchase_model - get revenue by singer'.$this->db->last_query());
+		$result = NULL;
+		if ($query->num_rows() > 0)
+		{
+		   foreach ($query->result_array() as $row)
+		   {
+		      $result[] = $row;
+		   }
+		}
+		log_message('info', 'purchase_model - result is '.print_r($result,TRUE));
+		return $result;
 	}
 
 	function getRevenueByGenre($genre){
@@ -65,7 +122,7 @@ class Purchase_model extends CI_Model {
 	function getRevenueByComposer($firstName, $lastName){
 		$SQL = "SELECT ccs.ccsComposerFirstName, ccs.ccsComposerLastName, SUM(p.amountPaid) FROM composercomposessong ccs, purchases p WHERE 
 				ccs.ccsAlbumTitle = p.pAlbumTitle AND ccs.ccsAlbumYear = p.pAlbumYear AND
-				LOWER(ccs.ccsComposerFirstName) LIKE LOWER('%".$firstName."%') AND LOWER(ccs.ccs.ccsComposerLastName) LIKE LOWER('%".$lastName."%')
+				LOWER(ccs.ccsComposerFirstName) LIKE LOWER('%".$firstName."%') AND LOWER(ccs.ccsComposerLastName) LIKE LOWER('%".$lastName."%')
 				GROUP BY ccs.ccsComposerFirstName, ccs.ccsComposerLastName, SUM(p.amountPaid) ORDER BY SUM(p.amountPaid) DESC";
 
 		$query = $this->db->query($SQL);

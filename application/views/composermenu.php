29db616bfd893ec1804464dc4fdc47575b0b09c7
Steve Liu <steveliu.api@gmail.com>
2014-04-11 23:07:13 +0800

    Navigation in UI

--- application/views/composermenu.php
+++ application/views/composermenu.php
@@ -22,7 +22,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="../home">Welcome, Guest!</a>
+        <a class="navbar-brand" href="./home">Welcome, Guest!</a>
       </div>
       <div class="navbar-collapse collapse">
         <ul class="nav navbar-nav navbar-right">

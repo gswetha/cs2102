33c7a48daa4ed74a195bed716ca313b44ae64971
gswetha <gswetha@hotmail.com>
2014-04-14 21:53:55 +0800

    Add Logout links in menu

--- application/views/admin_edit.php
+++ application/views/admin_edit.php
@@ -36,7 +36,7 @@
                 <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
                   <li role="presentation"><a role="menuitem" tabindex="-1" >My Profile</a></li>
                   <li role="presentation"><a role="menuitem" tabindex="-1" href="../home/purchases">My Purchases</a></li>
-                  <li role="presentation" class="divider"></li>
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href=<?php echo $this->config->item('base_url')."userController/logout"?>>Logout</a></li>
                   
                 </ul>
             </li>
